5d4f49d9e0c69d8fb666ee331375c84e1352847e
tamilabagrationi <116660917+tamilabagrationi@users.noreply.github.com>
2025-03-01 11:18:21 +0400

    add hereri

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+
+let navigation = document.getElementById('navigation')
+let burger =document.getElementById('burger')
+burger.addEventListener('click', function(){
+    if(navigation.classList.contains('activeNavigation')){
+        navigation.classList.remove('activeNavigation')
+        burger.innerHTML = '<i class="fa-solid fa-bars"></i>'
+
+    }else{
+         navigation.classList.add('activeNavigation')
+        burger.innerHTML = '<i class="fas fa-times"></i>'
+
+    }
+})
