b3f9312774f948440ebc4538cca21fc27e76407a
tamilabagrationi <116660917+tamilabagrationi@users.noreply.github.com>
2025-03-18 19:05:45 +0400

    slaideri

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+"use strict"
 
 let navigation = document.getElementById('navigation')
 let burger =document.getElementById('burger')
@@ -12,3 +13,113 @@ burger.addEventListener('click', function(){
 
     }
 })
+
+
+
+let data = [
+    {
+        id: 1,
+        title:'my school',
+        imageUrl: 'http://salibauri2.edu.ge/images/pansioni/1.JPG'
+    },
+    {
+        id: 2,
+        title:'School alley',
+        imageUrl: 'https://tse3.mm.bing.net/th?id=OIP.yMSJfLrgunCHat-A_QlLUwHaFj&pid=Api&P=0&h=220',
+        
+    },
+    {
+        id: 3,
+        title:'Winners of the Battle of Brains',
+        imageUrl: 'https://www.etaloni.ge/gallery/ertretyr.jpg',
+        
+    },
+    {
+        id:4,
+        title:'Winner of the Educational-Creative Conferenceflower',
+        imageUrl:'https://www.etaloni.ge/gallery/88309salibaurismeoreeeee.jpg'
+
+    }
+
+    
+   
+];
+let sliderContainer = document.getElementById('slider')
+let arrowLeftBtn =document.getElementById('arrow-left')
+let arrowRightBtn=document.getElementById('arrow-right')
+
+let sliderIndex = 0
+
+function createImg (item){
+    sliderContainer.style.backgroundImage = `url(${item.imageUrl})`
+    sliderContainer.style.backgroundRepeat= 'no-repeat'
+    sliderContainer.style.backgroundSize= 'cover'
+ }
+ 
+function createH2tag(item){
+    let h2tag = document.createElement('h2')
+    h2tag.innerText =item.title
+    h2tag.classList.add('slider-title')
+    return h2tag
+
+}
+
+
+
+function setSlider(){
+    sliderContainer.innerText=''
+     createImg(data[sliderIndex])
+    let title= createH2tag(data[sliderIndex])
+    let dots = createDots()
+
+    sliderContainer.appendChild(title)
+    sliderContainer.appendChild(dots)
+    let dotElements = dots.querySelectorAll('.dot')
+    dotElements[sliderIndex].classList.add('active') 
+}
+
+function arrowLeft(){
+    if(sliderIndex === 0){
+        sliderIndex = data.length-1
+    }else{
+        sliderIndex --
+    }
+    setSlider()
+}
+
+function arrowRight(){
+    if(sliderIndex === data.length-1){
+        sliderIndex = 0
+    }else{
+        sliderIndex ++
+    }
+    setSlider()
+}
+arrowLeftBtn.addEventListener('click',arrowLeft)
+arrowRightBtn.addEventListener('click',arrowRight)
+
+// setInterval(()=>{
+//     arrowRight()
+// },2000)
+
+function createDots(){
+    let dots =document.createElement('div')
+    dots.classList.add('dots')
+    data.forEach((element)=>{
+        let dot = document.createElement('div')
+        dot.classList.add('dot')
+        dot.setAttribute('data-id',element.id-1)
+        dot.onclick= function(event){
+           let id = event.target.getAttribute('data-id')
+           sliderIndex = id
+           setSlider()
+        }
+        dots.appendChild(dot)
+
+    })
+
+    return dots
+
+}
+
+setSlider()
