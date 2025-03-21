d7d38f913b6c0a65629a2760c1fc2351f4fd4331
tamilabagrationi <116660917+tamilabagrationi@users.noreply.github.com>
2025-03-21 16:49:27 +0400

    acardion

--- script.js
+++ script.js
@@ -124,3 +124,17 @@ function createDots(){
 }
 
 setSlider()
+
+
+
+document.addEventListener('DOMContentLoaded', function(){
+    let acordion = document.getElementsByClassName('acordion-container')
+    
+    for(let i=0; i< acordion.length; i++){
+    acordion[i].addEventListener('click',function(){
+    this.classList.toggle('active')
+    console.log(acordion);
+      })
+         }
+    })   
+    
